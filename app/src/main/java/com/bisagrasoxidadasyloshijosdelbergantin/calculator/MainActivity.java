f9ba8370184848b09a7f94154b0ecabff34a790e
oalberto96 <oalberto96c@gmail.com>
2017-09-29 23:54:06 -0700

    Teclado calculadora

---
+++ app/src/main/java/com/bisagrasoxidadasyloshijosdelbergantin/calculator/MainActivity.java
@@ -0,0 +1,30 @@
+package com.bisagrasoxidadasyloshijosdelbergantin.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv_screen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv_screen = (TextView) findViewById(R.id.tv_screen);
+        setListeners();
+    }
+
+    protected void setListeners() {
+        Button btn_0 = (Button) findViewById(R.id.btn_0);
+        btn_0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv_screen.append("0");
+            }
+        });
+    }
+}
